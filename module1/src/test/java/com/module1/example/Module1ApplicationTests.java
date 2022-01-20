b1927ee0e6608f5241cd42a98df6f6430fd09519
Sumit Pareek <sumit@Sumits-MacBook-Air.local>
2022-01-21 00:59:51 +0530

    initial commit

---
+++ module1/src/test/java/com/module1/example/Module1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module1.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
