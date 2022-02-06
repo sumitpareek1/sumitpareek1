9817b11d0c8f264d14c0858b7b0d6aa5312a5fa6
Sumit Pareek <sumit@Sumits-MacBook-Air.local>
2022-02-06 22:24:23 +0530

    yoyo

--- module1/src/main/java/com/module1/example/Module1Application.java
+++ module1/src/main/java/com/module1/example/Module1Application.java
@@ -13,5 +13,6 @@ public class Module1Application {
     SpringApplication.run(Module1Application.class, args);
 
     System.out.println("111229992ldsf;sdkf;l111");
+    System.out.println("111229992ldsf;sdkf;l111");
   }
 }
