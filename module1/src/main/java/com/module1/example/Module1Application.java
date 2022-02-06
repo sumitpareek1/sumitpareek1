40a8cd67a183adaef3775987dcafdc27f4e2f211
Sumit Pareek <sumit@Sumits-MacBook-Air.local>
2022-02-06 20:30:04 +0530

    done

--- module1/src/main/java/com/module1/example/Module1Application.java
+++ module1/src/main/java/com/module1/example/Module1Application.java
@@ -6,27 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Module1Application {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		// yoyo yoyo
+    // yoyo yoyo
 
+    SpringApplication.run(Module1Application.class, args);
 
-
-
-
-
-
-
-
-
-
-		SpringApplication.run(Module1Application.class, args);
-
-
-		System.out.println("111229992111");
-
-
-
-	}
-
+    System.out.println("111229992111");
+  }
 }
--- module2/src/main/java/com/module2/example/Module2Application.java
+++ module2/src/main/java/com/module2/example/Module2Application.java
@@ -6,30 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Module2Application {
 
-	public static void main(String[] args) {
-		// yoyo
-		System.out.println("222288");
+  public static void main(String[] args) {
+    // yoyo
+    System.out.println("222288");
 
-
-
-
-
-
-
-
-
-
-		
-		
-		
-		
-
-
-
-
-
-
-		SpringApplication.run(Module2Application.class, args);
-	}
-
+    SpringApplication.run(Module2Application.class, args);
+  }
 }
